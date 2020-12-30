a25849eefdaaebbf8b02bc8be2dc9339a91758d5
Ajay Gokhale <ajay.g@callme.co.in>
2020-12-30 12:21:22 +0530

    Nested route completed. We can add route under route there is no problem with that.

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Posts from "./posts";
+import SideBar from "./sidebar";
+import Users from "./users";
+
 class Dashboard extends Component {
   state = {};
   render() {
-    return <h1>Dhashboard</h1>;
+    return (
+      <div>
+        <h1>Admin Dashboard</h1>
+        <SideBar />
+        <Route path="/admin/users" component={Users} />
+        <Route path="/admin/posts" component={Posts} />
+      </div>
+    );
   }
 }
 

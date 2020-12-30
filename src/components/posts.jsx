7c58887cc4cb19c6e06cd7adfd3aff6d41e0e838
Ajay Gokhale <ajay.g@callme.co.in>
2020-12-30 12:00:27 +0530

    Query String Passing and fetching using query-string npm package completed.

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import queryString from "query-string";
+
 class Posts extends Component {
   state = {};
   render() {
     const { year, month } = this.props.match.params;
+    const { location } = this.props;
+    const result = queryString.parse(location.search);
+    console.log(result);
 
     return (
-      <h3>
-        Posts year : {year === undefined ? "year Not Provided" : year}, month:
-        {month === undefined ? "month Not Provided" : month}
-      </h3>
+      <div>
+        <h3>
+          Posts year : {year === undefined ? "year Not Provided" : year}, month:
+          {month === undefined ? "month Not Provided" : month}
+        </h3>
+
+        <h4>{result.sortBy}</h4>
+        <h4>{result.approved}</h4>
+      </div>
     );
   }
 }
